0241d70fdb42a4de47aeb27fd68b6b52684fd8c4
shanushka <anushkashrestha24@gmail.com>
2019-01-15 12:37:30 +0545

    Updates

---
+++ precache-manifest.f733c8bc4ea4ba02e6ea69debac7b47c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "43c276e7e305b43f1822",
+    "url": "/TodoList/static/css/main.559fbadc.chunk.css"
+  },
+  {
+    "revision": "43c276e7e305b43f1822",
+    "url": "/TodoList/static/js/main.43c276e7.chunk.js"
+  },
+  {
+    "revision": "a3b7f30c0a2c77ba46ba",
+    "url": "/TodoList/static/js/1.a3b7f30c.chunk.js"
+  },
+  {
+    "revision": "e5fd00049e7ac7cce407",
+    "url": "/TodoList/static/js/runtime~main.e5fd0004.js"
+  },
+  {
+    "revision": "c985d74facae8b29d567faa72e0b57b3",
+    "url": "/TodoList/static/media/search.c985d74f.png"
+  },
+  {
+    "revision": "a6239530cdc97ba8f79065de02f403b2",
+    "url": "/TodoList/static/media/edit.a6239530.png"
+  },
+  {
+    "revision": "4d1ffe400493dec3cee9ccbdeb7fa019",
+    "url": "/TodoList/static/media/delete.4d1ffe40.png"
+  },
+  {
+    "revision": "38bef3c8c03e2706b7aa86bb1a39d73c",
+    "url": "/TodoList/static/media/complete.38bef3c8.png"
+  },
+  {
+    "revision": "614606419698e472f8b3ca1ed8efae41",
+    "url": "/TodoList/index.html"
+  }
+];
